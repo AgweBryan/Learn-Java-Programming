36b1f2f6721ef415ea6e83e7337e79d28086023a
Neba-Emmanuel <nebaemmanuel736@gmail.com>
2021-09-10 07:22:14 +0100

    newCommit

---
+++ Pattern Programs/leftTrianglePattern.java
@@ -0,0 +1,21 @@
+//Done by Neba Emmanuel
+//Look me up on https://github.com/Neba-Emmanuel/
+       
+public class main leftTrianglePattern{
+    public static void main(String args[]){
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j <= i; j++){
+            System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*      OutPut
+        *  
+        *  *
+        *  *  *
+        *  *  *  * 
+        *  *  *  *  *
+*\  
---
+++ Pattern Programs/rightTrianglePattern.java
@@ -0,0 +1,26 @@
+//Done by Neba Emmanuel
+//Look me up on https://github.com/Neba-Emmanuel/
+       
+public class main squarePattern{
+    public static void main(String args[]){
+        int n = 5;
+        for(int i = 1; i <= n; i++){
+            for(int j = n-1; j >= i; j--){
+            System.out.println("");
+            }
+            for(int k = 1; k <= 1; k++){
+            System.out.println("*"); 
+            }
+            System.out.println();
+        }       
+        
+    }
+}
+
+/*      OutPut
+                    *
+                 *  *
+              *  *  *
+           *  *  *  *
+        *  *  *  *  *
+*\  
